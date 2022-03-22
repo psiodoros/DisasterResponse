cf76092e47e256ef2d372694980bde3644022371
Pavlos Siontoros <psiodoros@gmail.com>
2022-03-22 16:40:04 +0200

    Cleaned up files

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\p.siontoros\Documents\Python Scripts\Udacity\DisasterResponse\.venv
+set VIRTUAL_ENV=C:\Users\p.siontoros\Documents\PythonScripts\Udacity\DisasterResponse\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
